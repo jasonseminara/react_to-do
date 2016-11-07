ed110dedb850fbda7ef85404501f0c51f0d4730a
Jason Seminara <jseminara@gmail.com>
2016-11-07 13:02:22 -0500

    added morning lesson

---
+++ routes/tasks.js
@@ -0,0 +1,20 @@
+const tasks = require('express').Router();
+const db = require('../model/task');
+
+/* convenience method for sending */
+const sendJSONresp = (req, res) => res.json(res.rows);
+
+// tasks/:id
+// this is more specific than the /tasks, so it goes above
+tasks.route('/:taskID')
+  .put((req, res) => res.json(`put tasks/${req.params.taskID}`))
+  .delete((req, res) => res.json(`delete tasks/${req.params.taskID}`));
+
+// tasks
+// this is the most general route, so it goes last
+tasks.route('/')
+  .get(db.getTasks, sendJSONresp)
+  .post(db.addTask, sendJSONresp);
+
+// export this so it is available to server.js
+module.exports = tasks;
--- server.js
+++ server.js
@@ -1,7 +1,3 @@
-/* eslint no-multi-spaces: ["error", { exceptions: { "VariableDeclarator": true } }] */
-/* eslint import/no-extraneous-dependencies: ["error", {"devDependencies": true}] */
-/* eslint strict: ["error", {"global": true}] */
-
 'use strict';
 
 // regular stuff
@@ -11,10 +7,10 @@ const logger      = require('morgan');
 
 // This tests to see if we have NODE_ENV in our environment.
 // Only load the dotenv if we need it.
-const isDev       = !('NODE_ENV' in process.env) && require('dotenv').config() && true;
+const isDev = !('NODE_ENV' in process.env) && require('dotenv').config() && true;
 
-const app         = express();
-const PORT        = process.argv[2] || process.env.port || 3000;
+const app    = express();
+const PORT   = process.argv[2] || process.env.port || 3000;
 
 // set up some logging
 app.use(logger(isDev ? 'dev' : 'common'));
@@ -22,11 +18,16 @@ app.use(logger(isDev ? 'dev' : 'common'));
 // we're only going to accept json
 app.use(bodyParser.json());
 
+// bring in the task routes
+app.use('/tasks', require('./routes/tasks'));
+app.use('/', require('./routes/index'));
+
+// generic error handler
 app.use((err, req, res, next) => {
-  res.status(500).send('Something broke!', next);
+  console.error(err);
+  res.status(500).send('Something broke!');
 });
 
 // Let's go!
-app.listen(PORT, () => {
-  console.log(process.env, isDev);
-});
+app.listen(PORT);
+// app.listen(PORT, () => console.log(Object.keys(process.env).sort(), isDev));
